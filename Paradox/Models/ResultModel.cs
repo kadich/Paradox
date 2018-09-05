c2502a028c74393af820366000f1dfb30890e931
kadich <35394916+kadich@users.noreply.github.com>
2018-09-06 00:57:13 +0300

    Minor changes

--- Paradox/Models/ResultModel.cs
+++ Paradox/Models/ResultModel.cs
@@ -7,7 +7,7 @@ namespace Paradox.Models
 {
     public class ResultModel
     {
-        public int WinRate1 { get; set; } //свойсво для записи побед стратегии №1
-        public int WinRate2 { get; set; } //свойсво для записи побед стратегии №1
+        public int WinRate1 { get; set; } //свойство для записи побед стратегии №1
+        public int WinRate2 { get; set; } //свойство для записи побед стратегии №2
     }
 }
